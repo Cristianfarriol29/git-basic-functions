cacc397968aa822ca0b85fd60a592fdf14d3c160
cristian <cristianfarriol19@hotmail.com>
2022-02-28 19:00:25 +0100

    tercera iteracion

--- iteracion3.js
+++ iteracion3.js
@@ -1,21 +1,20 @@
-const numbers = [1, 2, 3, 5, 45, 37, 58];
-
-function sumAll(param) {
-
+const numbers = [12, 21, 38, 5, 45, 37, 6];
+function average(param) {
+  
     let accumulator = 0;
+    for (let i = 0; i < param.length; i++){
 
 
-    param.forEach(number => {
+        accumulator += param[i]
 
-     accumulator += number;
+    }
 
-     
+    promedio = accumulator / param.length
 
-    })
+    return console.log(promedio)
 
-    return(console.log(accumulator))
 
 }
 
 
-sumAll(numbers)
+average(numbers)
